ad603376ad18aeefc0ca871660e384b1c4fcd2a6
Camden Jones <cj00642@surrey.ac.uk>
2024-07-05 18:14:28 +0100

    manually verify sbg guild members

--- database/db.js
+++ database/db.js
@@ -1,6 +1,7 @@
 const {FSDB} = require("file-system-db");
 const db = new FSDB("./database/members.json", false);
 
+//makes the player array if not run before
 if (!db.has("players")) {
     db.set("players", []);
 }
---
+++ manualVerify.js
@@ -0,0 +1,75 @@
+const db = require("./database/db.js").getMembers();
+const {hypixelAPI} = require("./config.json")
+
+/**
+ * 
+ * @returns All members in the guild
+ */
+
+async function guildCall() {
+    const response = await fetch(`https://api.hypixel.net/v2/guild?id=5fea32eb8ea8c9724b8e3f3c`, {
+        method: "GET",
+        headers: {
+            "Api-Key": hypixelAPI
+        },
+    });
+    return await response.json();
+}
+
+/**
+ * Gets playerdata from hypixel api from uuid
+ * @param {string} uuid 
+ * @returns {Object} data
+ */
+async function fetchHypixel(uuid) {
+    const response = await fetch(`https://api.hypixel.net/v2/player?uuid=${uuid}`, {
+        method: "GET",
+        headers: {
+            "Api-Key": hypixelAPI
+        },
+    });
+    return await response.json();
+}
+
+async function memberCall(data) {
+    const playerData = [];
+    for (let i = 0; i < data.guild.members.length; i++) {
+        playerData.push({
+            uuid: data.guild.members[i].uuid,
+            rank: data.guild.members[i].rank
+        });
+    }
+    return playerData;
+}
+
+async function main() {
+    const allMembers = await guildCall();
+    let allUUIDS = await memberCall(allMembers);
+
+    for (let player of allUUIDS) {
+        let hypixel = await fetchHypixel(player.uuid);
+        let minecraftName = hypixel.player.playername
+        let discord;
+        if (hypixel.player.socialMedia === undefined || hypixel.player.socialMedia.links === undefined || !hypixel.player.socialMedia.links.DISCORD) {
+            console.log(`${hypixel.player.playername} doesnt have discord linked`)
+            discord = "HELLO I HAVE NO DISCORD"
+        } else {
+            discord = hypixel.player.socialMedia.links.DISCORD
+        }
+        
+        player.discord = discord;
+        player.minecraftName = minecraftName;
+
+    }
+
+    for (let player of allUUIDS) {
+        db.push("players", {
+            discord: player.discord,
+            minecraftName: player.minecraftName,
+            minecraftUUID: player.uuid,
+            guild: true
+        })
+    }
+}
+
+main();
