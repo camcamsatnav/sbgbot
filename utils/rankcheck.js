155b0c5117fd3d2d2b86bdbc6231c4d99a5a92b6
Camden Jones <cj00642@surrey.ac.uk>
2024-07-05 18:18:10 +0100

    main logic for member role checking

--- utils/rankcheck.js
+++ utils/rankcheck.js
@@ -1,13 +1,42 @@
-const {db} = require('./../database/members.json');
+const db = require('./../database/db.js').getMembers();
+const {hypixelAPI} = require('./../config.json');
 
-async function main() {
+/**
+ * Adds and removes guild member roles
+ * @param members people with guild member role 
+ */
+async function main(members) {
     const guildData = await guildCall();
     const memberData = await memberCall(guildData);
 
-    for (const member in memberData) {
-        const player = db.get("players").find(player => player.minecraftUUID === member.uuid);
-        
-    }
+    //first sync the db to match ingame
+    await db.get("players").forEach(player => {
+        if (player.guild === false && !memberData.find(member => member.uuid === player.minecraftUUID)) {
+            console.log(`${player.minecraftUUID} is not in the guild and not in the db`); //fine
+        } else if (player.guild === true && memberData.find(member => member.uuid === player.minecraftUUID)) {
+            console.log(`${player.minecraftUUID} is in the guild and in the db`); //fine
+        } else if (player.guild === false && memberData.find(member => member.uuid === player.minecraftUUID)) {
+            console.log(`${player.minecraftUUID} is in the guild and not in the db`); //ie joined but not in db
+            player.guild = true; //sets the database to say they are in the guild
+        } else if (player.guild === true && !memberData.find(member => member.uuid === player.minecraftUUID)) {
+            console.log(`${player.minecraftUUID} not in the guild and is in the db`); //ie they left
+            player.guild = false; //sets database to say not in the guild
+        }
+    });
+    //then sync roles to match the db
+    await db.get("players").forEach(player => {
+        if (player.guild === true && members.find(m => m.user.username === player.discord)) {
+            //in guild and has role
+        } else if (player.guild === false && members.find(m => m.user.username === player.discord)) {
+            // not in guild but has guild member role therefore remove role on discord
+            console.log(`i am removing guild memebr role from ${player.minecraftName}`)
+        } else if (player.guild === true && !members.find(m => m.user.username === player.discord)) {
+            //in guild but doesnt have role therefore add role on discord
+            console.log(`i am adding guild memebr role to ${player.minecraftName}`)
+        } else if (player.guild === false && !members.find(m => m.user.username === player.discord)) {
+            //not in guild and doent have role
+        }
+    });
 }
 
 
@@ -15,7 +44,7 @@ async function guildCall() {
     const response = await fetch(`https://api.hypixel.net/v2/guild?id=5fea32eb8ea8c9724b8e3f3c`, {
         method: "GET",
         headers: {
-            "Api-Key": "e48ec7a3-df20-409c-954e-f4537044cce1"
+            "Api-Key": hypixelAPI
         },
     });
     return await response.json();
@@ -31,3 +60,5 @@ async function memberCall(data) {
     }
     return playerData;
 }
+
+module.exports = {main}
